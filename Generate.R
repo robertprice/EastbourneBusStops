eff533c017ecabacc772532dc3ea62152f0225b6
Robert Price <robert.price@gmail.com>
2018-04-17 21:09:14 +0100

    Added the location of all bus stops in Eastbourne

---
+++ Generate.R
@@ -0,0 +1,14 @@
+# Download stops from NaPTAN - naptan.app.dft.gov.uk/DataRequest/Naptan.ashx?format=csv&LA=150
+# The LA code for East Sussex where Eastborne is located is 140, codes for other local authorities can be found at
+# http://naptan.app.dft.gov.uk/Reports/frmStopsSummaryReport
+
+library(readr)
+Stops <- read_csv("~/Downloads/NaPTAN140csv/Stops.csv",
+    col_types = cols(CreationDateTime = col_datetime(format = "%Y-%m-%dT%H:%M:%S"),
+        ModificationDateTime = col_datetime(format = "%Y-%m-%dT%H:%M:%S")))
+
+# filter out only stops in Eastbourne.
+EastbourneStops <- Stops[Stops$ParentLocalityName=="Eastbourne" & !is.na(Stops$ParentLocalityName),]
+
+# write these out to a fresh CSV file.
+write.csv(EastbourneStops, file = "~/Desktop/EastbourneStops.csv", na="", row.names = FALSE)
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # EastbourneBusStops
 The location of bus stops in Eastbourne, East Sussex
+
+## Generate the stops CSV file
+
+Download the bus stops for East Sussex from naptan.app.dft.gov.uk/DataRequest/Naptan.ashx?format=csv&LA=150 then run Generate.R to create the CSV file
+
+## Plot the stops on a map
+
+Can plot the stops onto a map if you use the ggmap library. This assumes you have loaded the location of the bus stops into EastbourneStops
+
+```R
+library(ggmap)
+eastbourne2_lvl13_map_g_str <- get_map(location = c(lon=0.2929012,  lat=50.7896285), zoom = 13)
+ggmap(eastbourne2_lvl13_map_g_str, extent = "device") + geom_point(data=EastbourneStops, mapping=aes(y=Latitude, x=Longitude), color = "red", size = 2, show.legend=FALSE)
+```
